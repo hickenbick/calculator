580c49faec2e1e0538becf164f31d4e1dfb5192c
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-02-08 13:48:26 +0000

    classes for numbers, operators and other buttons changed. function operate refactored

--- script.js
+++ script.js
@@ -1,71 +1,36 @@
 const btns = document.querySelectorAll(".btn");
-const visor = document.querySelector(".visor")
+const visor = document.querySelector(".visor");
 let visorContent = "";
 
-// listen to all buttons clicked
-btns.forEach(function(btn){
-  btn.addEventListener("click", function() {
-    console.log(btn.textContent)
-    updateVisor(btn.textContent)
-  })
-})
 
 // update visor on every button clicked
 function updateVisor(input) {
-  visorContent += input
-  visor.textContent = visorContent;
+  
 }
 
 // operate numbers
-function add(n0, n1) {
-  return (+n0 + +n1);
-} 
-
-function subtract(n0, n1) {
-  return (n0 - n1);
-}
-
-function multiply(n0, n1) {
-  return (n0 * n1);
-}
-
-function divide(n0, n1) {
-  return (n0 / n1);
-}
 
 // round numbers to 2 digits
 function round(value, precision) {
-  var multiplier = Math.pow(10, precision || 2);
+  var multiplier = Math.pow(10, precision || 5);
   return Math.round(value * multiplier) / multiplier;
 }
 
 // return right operator
-function operate(op, n0, n1) {
+function operate(op, prevNum, currNum) {
   if (op === "+") {
-    return round(add(n0, n1));
+    return round((Number(prevNum) + Number(currNum)));
   }
   if (op === "-") {
-    return round(subtract(n0, n1));
+    return round(prevNum - currNum);
   }
   if (op === "*") {
-    return round(multiply(n0, n1));
+    return round(prevNum * currNum);
   }
   if (op === "/") {
-    return round(divide(n0, n1));
+    return round(prevNum / currNum);
   }
   else {
     return "ERR"
   }
 }
-
-console.log("test")
-
-// tests
-// let plus = operate("+", 2.5555555, 2);
-// let minus = operate("-", 2.545, 2);
-// let multi = operate("*", 5324, 2.12);
-// let div = operate("/", 10.2234, 2);
-// console.log(plus)
-// console.log(minus)
-// console.log(multi)
-// console.log(div)
